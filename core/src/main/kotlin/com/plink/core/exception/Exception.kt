0751837fbddfc6e1a20f4fa2c8431fe7eb9e2575
chobolevel <rodaka123@naver.com>
2026-03-12 18:16:36 +0900

    fix: lint issue

--- core/src/main/kotlin/com/plink/core/exception/Exception.kt
+++ core/src/main/kotlin/com/plink/core/exception/Exception.kt
@@ -10,28 +10,28 @@ open class InvalidParameterException(
     override val code: String,
     override val message: String,
     override val throwable: Throwable? = null
-): PlinkException(code, message, throwable)
+) : PlinkException(code, message, throwable)
 
 open class PolicyViolationException(
     override val code: String,
     override val message: String,
     override val throwable: Throwable? = null
-): PlinkException(code, message, throwable)
+) : PlinkException(code, message, throwable)
 
 open class UnAuthorizedException(
     override val code: String,
     override val message: String,
     override val throwable: Throwable? = null
-): PlinkException(code, message, throwable)
+) : PlinkException(code, message, throwable)
 
 open class ForbiddenException(
     override val code: String,
     override val message: String,
     override val throwable: Throwable? = null
-): PlinkException(code, message, throwable)
+) : PlinkException(code, message, throwable)
 
 open class DataNotFoundException(
     override val code: String,
     override val message: String,
     override val throwable: Throwable? = null
-): PlinkException(code, message, throwable)
+) : PlinkException(code, message, throwable)
