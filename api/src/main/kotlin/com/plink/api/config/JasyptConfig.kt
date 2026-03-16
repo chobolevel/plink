f3022f20aa35ac18cba86f9ff09ea7a0df4c402f
chobolevel <rodaka123@naver.com>
2026-03-16 18:57:50 +0900

    feat: JASYPT 적용
    - 설정 파일 민감 정보 암호화 관리

---
+++ api/src/main/kotlin/com/plink/api/config/JasyptConfig.kt
@@ -0,0 +1,28 @@
+package com.plink.api.config
+
+import com.ulisesbocchio.jasyptspringboot.annotation.EnableEncryptableProperties
+import org.jasypt.encryption.StringEncryptor
+import org.jasypt.encryption.pbe.PooledPBEStringEncryptor
+import org.jasypt.encryption.pbe.config.SimpleStringPBEConfig
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
+
+@Configuration
+@EnableEncryptableProperties
+class JasyptConfig {
+
+    @Bean("jasyptStringEncryptor")
+    fun stringEncryptor(): StringEncryptor {
+        val encryptor = PooledPBEStringEncryptor()
+        val config = SimpleStringPBEConfig()
+        config.password = "chobolevel-plink"
+        config.algorithm = "PBEWithMD5AndDES"
+        config.setKeyObtentionIterations("1000")
+        config.setPoolSize("1")
+        config.providerName = "SunJCE"
+        config.setSaltGeneratorClassName("org.jasypt.salt.RandomSaltGenerator")
+        config.stringOutputType = "base64"
+        encryptor.setConfig(config)
+        return encryptor
+    }
+}
