6d06b000133ca3e3d376ca0b4c912b50e45e7676
chobolevel <rodaka123@naver.com>
2026-03-16 11:14:29 +0900

    fix: remove duplicated dependency & config

--- api/build.gradle.kts
+++ api/build.gradle.kts
@@ -7,12 +7,14 @@ plugins {
 }
 
 dependencies {
-    implementation("org.jetbrains.kotlin:kotlin-reflect")
+    // 프로그램 실행 중에 자기 자신의 구조(클래스 이름, 프로퍼티 목록, 어노테이션 정보 등)을 들여다보고 조작할 수 있게 해주는 라이브러리
+    // JSON 직렬화/역직렬화, JPA/Hibernate, 어노테이션 기반 로직 등에 필요
     implementation(project(":user"))
+    implementation("org.jetbrains.kotlin:kotlin-reflect")
     implementation("org.springframework.boot:spring-boot-starter-web")
-    implementation("org.springframework.boot:spring-boot-starter-data-jpa")
     implementation("org.springframework.boot:spring-boot-starter-security")
     implementation("org.springdoc:springdoc-openapi-starter-webmvc-ui:2.8.16")
+
     testImplementation("org.springframework.security:spring-security-test")
 
     runtimeOnly("com.mysql:mysql-connector-j")
--- jpa/src/main/kotlin/com/plink/envers/config/JpaConfiguration.kt
+++ jpa/src/main/kotlin/com/plink/envers/config/JpaConfiguration.kt
@@ -1,14 +1,18 @@
 package com.plink.envers.config
 
+import org.springframework.boot.autoconfigure.domain.EntityScan
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.auditing.DateTimeProvider
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories
 import java.time.OffsetDateTime
 import java.util.Optional
 
 @Configuration
 @EnableJpaAuditing(dateTimeProviderRef = "auditingDateTimeProvider")
+@EnableJpaRepositories(basePackages = ["com.plink"])
+@EntityScan(basePackages = ["com.plink"])
 class JpaConfiguration {
 
     @Bean
