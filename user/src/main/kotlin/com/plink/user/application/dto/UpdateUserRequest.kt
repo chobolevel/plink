0385e238a402de35b433f182c75063c57f04bbcf
chobolevel <rodaka123@naver.com>
2026-03-18 14:01:34 +0900

    fix: add validation dto property by updateMask

--- user/src/main/kotlin/com/plink/user/application/dto/UpdateUserRequest.kt
+++ user/src/main/kotlin/com/plink/user/application/dto/UpdateUserRequest.kt
@@ -7,4 +7,10 @@ data class UpdateUserRequest(
     val nickname: String?,
     @field:Size(min = 1)
     val updateMask: List<UserUpdateMask>
-)
+) {
+    init {
+        if (UserUpdateMask.NICKNAME in updateMask) {
+            require(!nickname.isNullOrEmpty()) { "nickname은(는) 필수 값입니다." }
+        }
+    }
+}
