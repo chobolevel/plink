5b27aa5c642d2606cb3d0729f02427d3667b069a
chobolevel <rodaka123@naver.com>
2026-04-14 12:01:02 +0900

    fix: 중복 검증 제거

--- user/src/main/kotlin/com/plink/user/application/dto/UpdateUserRequest.kt
+++ user/src/main/kotlin/com/plink/user/application/dto/UpdateUserRequest.kt
@@ -7,10 +7,4 @@ data class UpdateUserRequest(
     val nickname: String?,
     @field:Size(min = 1)
     val updateMask: List<UserUpdateMask>
-) {
-    init {
-        if (UserUpdateMask.NICKNAME in updateMask) {
-            require(!nickname.isNullOrEmpty()) { "nickname은(는) 필수 값입니다." }
-        }
-    }
-}
+)
