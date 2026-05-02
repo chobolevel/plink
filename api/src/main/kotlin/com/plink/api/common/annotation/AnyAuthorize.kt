f420354abcbde4ed0bb444c6547c16ea651474ac
chobolevel <rodaka123@naver.com>
2026-05-02 17:59:32 +0900

    refactor: remove useless annotation

--- api/src/main/kotlin/com/plink/api/common/annotation/AnyAuthorize.kt
+++
@@ -1,8 +0,0 @@
-package com.plink.api.common.annotation
-
-import org.springframework.security.access.prepost.PreAuthorize
-
-@Retention(AnnotationRetention.RUNTIME)
-@Target(AnnotationTarget.FUNCTION)
-@PreAuthorize("hasAnyRole('USER', 'ADMIN')")
-annotation class AnyAuthorize()
